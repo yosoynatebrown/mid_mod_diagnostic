29586e0c19982d0b8775ffa804e47b8065622c76
Nathan Brown <nathanbrown@Nathans-MacBook-Pro.local>
2021-09-09 08:02:52 -0600

    Add iteration 2

--- lib/department.rb
+++ lib/department.rb
@@ -1,8 +1,19 @@
 require './lib/employee'
 
 class Department
-  attr_reader :name
+  attr_reader :name,
+              :employees,
+              :expenses
   def initialize(name)
     @name = name
+    @employees = []
+    @expenses = 0
+  end
+
+  def hire(employee)
+    @employees << employee
+  end
+  def expense(cost)
+    @expenses += cost
   end
 end
--- spec/department_spec.rb
+++ spec/department_spec.rb
@@ -4,6 +4,8 @@ require "./lib/department"
 describe Department do
   before(:each) do
     @customer_service = Department.new('Customer Service')
+    @bobbi = Employee.new({name: "Bobbi Jaeger", age: "30", salary: "100000"})
+    @aaron = Employee.new({name: "Aaron Tanaka", age: "25", salary: "90000"})
   end
   it 'exists' do
     expect(@customer_service).to be_a Department
@@ -14,4 +16,18 @@ describe Department do
   it 'has employees' do
     expect(@customer_service.employees).to eq([])
   end
+  it 'can hire employees'do
+    @customer_service.hire(@bobbi)
+    @customer_service.hire(@aaron)
+
+  expect(@customer_service.employees).to include(@aaron, @bobbi)
+  end
+  it 'can track expenses'do
+  expect(@customer_service.expenses).to eq(0)
+
+  @customer_service.expense(100)
+  @customer_service.expense(25)
+
+  expect(@customer_service.expenses).to eq(125)
+end
 end
